aa1d3878df169f3a2684a7f74af8c6986d4a81d1
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2019-11-11 19:52:28 +0100

    +

---
+++ Testingstuff/src/Hours.java
@@ -0,0 +1,32 @@
+public class Hours
+{
+  private int startWork;
+  private int endWork;
+
+  public Hours(int startWork,int endWork,Date date){
+    this.startWork=startWork;
+    this.endWork=endWork;
+
+  }
+
+  public int getStartWork()
+  {
+    return startWork;
+  }
+
+  public void setStartWork(int startWork)
+  {
+    this.startWork = startWork;
+  }
+
+  public int getEndWork()
+  {
+    return endWork;
+  }
+
+  public void setEndWork(int endWork)
+  {
+    this.endWork = endWork;
+  }
+  6516655222136
+}
